6d37337f34934c93c463bd6e246939c53f1ef24a
VisionaryAI Labs <visionaryailabs1@gmail.com>
2025-02-16 22:14:58 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     https://script.google.com/macros/s/AKfycbzOfI8xEXTrdUQPOFndXm6ud0tYQpTnzu6StEu0UHI/dev
     const scriptURL = 'hhttps://script.google.com/macros/s/AKfycbzOfI8xEXTrdUQPOFndXm6ud0tYQpTnzu6StEu0UHI/dev/exec';
 
-    // Send data to Google Apps Script
+// Send data to Google Apps Script
     fetch(scriptURL, {
         method: 'POST',
         body: JSON.stringify(formData),
@@ -21,8 +21,14 @@ document.getElementById('contactForm').addEventListener('submit', function(event
             'Content-Type': 'application/json'
         }
     })
-    .then(response => response.text())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
     .then(data => {
+        console.log('Success:', data);
         alert('Thank you for contacting Visionary AI Labs! We will get back to you soon.');
         document.getElementById('contactForm').reset(); // Clear the form
     })
@@ -31,4 +37,10 @@ document.getElementById('contactForm').addEventListener('submit', function(event
         alert('Something went wrong. Please try again.');
     });
 });
+
+
+
+
+
+
 
